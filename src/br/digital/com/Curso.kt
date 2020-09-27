6c690eedb6f19b8de0cd1a6c35d8bf535790b6b7
Ytalo Ribeiro <ytalordsp@ymail.com>
2020-09-26 20:44:08 -0400

    Parte E - Mudança na Classe Curso

--- src/br/digital/com/Curso.kt
+++ src/br/digital/com/Curso.kt
@@ -1,6 +1,11 @@
 package br.digital.com
 
-data class Curso(val nome: String, val codigoDeCurso: Integer) {
+class Curso(val nome: String,
+                 val codigoDeCurso: Integer, 
+                 val profTitular: ProfTitular, 
+                 val profAdjunto: ProfAdjunto, 
+                 val maxAlunos: Integer, 
+                 vararg val lista: Aluno) {
     override fun equals(other: Any?): Boolean {
         if(other !is Curso) return false
         if(codigoDeCurso != other.codigoDeCurso) return false
--- src/br/digital/com/Teste.kt
+++ src/br/digital/com/Teste.kt
@@ -1,16 +1,15 @@
+@file:Suppress("DEPRECATION")
+
 package br.digital.com
-
+//arquivo para realizar testes de cada parte adicionada, não pertencente ao original
 fun main(){
-    val P1 = ProfTitular("Jorge", "Ben", Integer(15), Integer(33), "Historia")
-    val P2 = ProfTitular("Alcione", "Assis", Integer(5), Integer(25), "Geografia")
-    val P3 = ProfAdjunto("Jorge", "Aragão", Integer(3), Integer(27), Integer(20))
-    val P4 = ProfAdjunto("Arlindo", "Cruz", Integer(9), Integer(33), Integer(15))
-    println(P1.equals(P2))
-    println(P1.equals(P3))
-    println(P1.equals(P4))
+    val p1 = ProfTitular("Jorge", "Ben", Integer(15), Integer(33), "Historia")
+    val p2 = ProfAdjunto("Jorge", "Aragão", Integer(3), Integer(27), Integer(20))
 
-    println(P1.especialidade)
-    println(P2.especialidade)
-    println(P3.hrsMonitoria)
-    println(P4.hrsMonitoria)
+    val a1 = Aluno("Ytalo", "Ribeiro", Integer(1123))
+    val a2 = Aluno("Roberto", "Carlos", Integer(1178))
+    val c1 = Curso("Geografia", Integer(123), p1, p2, Integer(20), a1, a2)
+    c1.lista.forEach { println(it.nome)}
+    println(c1.profTitular.nome)
+    println(c1.profAdjunto.nome)
 }
